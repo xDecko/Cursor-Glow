48a0e1167bf6b0fe2530e608f109d77e3c5d7330
Decko <deckocol@gmail.com>
2023-06-17 01:32:10 -0500

    Background

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+//CURSOR LIGHT
+
+window.onpointermove = event => {
+    const { clientX, clientY } = event;
+  
+    glow.style.left = `${clientX}px`; /* Utiliza 'style.left' en lugar de 'animate' */
+    glow.style.top = `${clientY}px`; /* Utiliza 'style.top' en lugar de 'animate' */
+  };
+  
+  //LIGHT EFFECTS
+  
+  function changeGlowColor() {
+    var glow = document.getElementById("glow");
+    glow.classList.add("hover");
+  }
+  
+  function resetGlowColor() {
+    var glow = document.getElementById("glow");
+    glow.classList.remove("hover");
+  }
+  
+  
